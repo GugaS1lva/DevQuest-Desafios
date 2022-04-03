9b61f41850f72e28ef39b4669e5e143832f8ff34
Gustavo Silva <92289607+GugaS1lva@users.noreply.github.com>
2022-04-03 08:20:00 -0300

    Fim do Desafio.

---
+++ Desafio-08/index.js
@@ -0,0 +1,3 @@
+let nome = window.prompt('Informe seu nome abaixo (ps: não tenho muito controle sobre os dados ainda então não poderei fazer nada se você digitar um monte de letras ou números aleatórios. Infome um nome legível por favor 😅):')
+
+window.alert(`É um prazer te conhecer, ${nome}!`)
